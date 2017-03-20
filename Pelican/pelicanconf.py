d3259dcf70a585c3465b938227b8dbee7297c817
Nils Giordano <nils.giordano@ls2n.fr>
2017-03-20 15:52:19 +0100

    Update Pelican, rework everything

--- Pelican/pelicanconf.py
+++ Pelican/pelicanconf.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Nils Giordano'
 
-SITENAME = 'Nils Giordano'
+SITENAME = "Yet Another Dr."
 SITEURL = 'http://www.normalesup.org/~giordano'
 TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = 'en'
-DEFAULT_CATEGORY = 'Misc'
+#DEFAULT_CATEGORY = 'Misc'
 
 PATH = '../raw'
 OUTPUT_PATH = '../WWW'
@@ -21,25 +21,25 @@ EXTRA_PATH_METADATA = {
 RELATIVE_URLS = True
 
 PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['extract_toc','render_math','better_figures_and_images']
-MD_EXTENSIONS = ['codehilite','extra','smarty', 'toc']
+PLUGINS = ['i18n_subsites']
+MARKDOWN = {
+        'extension_configs': {
+                    'markdown.extensions.codehilite': {'css_class': 'highlight'},
+                    'markdown.extensions.extra': {},
+                    'markdown.extensions.smarty': {},
+                    'markdown.extensions.toc': {},
+                },
+        'output_format': 'html5',
+}
 RESPONSIVE_IMAGES = True
 
 
 # Theme configuration
-THEME = 'themes/pelican-bootstrap3'
-BOOTSTRAP_THEME = 'simplex'
+THEME = '../../pelican-themes/pelican-bootstrap3'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+BOOTSTRAP_THEME = 'spacelab'
 BOOTSTRAP_NAVBAR_INVERSE = False
 FAVICON = 'extra/favicon.png'
-#ABOUT_ME = '<i class="fa fa-envelope-o" aria-hidden="true"></i>\
-#        <a href="http://www.google.com/recaptcha/mailhide/d?k=01vxJflfgEQgNevK72lauzLg==&amp;c=yqWFIeJ0ACeMcuh8CgQ1S3W_cX_5CqtVKYFcN1-8uUA=" onclick="window.open(\'http://www.google.com/recaptcha/mailhide/d?k\x3d01vxJflfgEQgNevK72lauzLg\x3d\x3d\x26c\x3dyqWFIeJ0ACeMcuh8CgQ1S3W_cX_5CqtVKYFcN1-8uUA\x3d', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300\'); return false;" title="Reveal this e-mail address">n...</a>@inria.fr\
-#        <i class="fa fa-phone" aria-hidden="true"></i> +33 (0)476615300<br />\
-#        <br />\
-#        Inria Grenoble − Rhône-Alpes<br />\
-#        Inovallée<br />\
-#        655 avenue de l\'Europe<br />\
-#        Montbonnot<br />\
-#        38334 Saint-Ismier Cedex, France'
 ABOUT_ME = '<i class="fa fa-envelope-o" aria-hidden="true"></i>\
         <script type="text/javascript" src="extra/email.js"></script>\
         <noscript>[Enable JavaScript to see my email address]</noscript><br />\
@@ -56,7 +56,7 @@ DISPLAY_CATEGORIES_ON_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 PAGE_ORDER_BY = 'rank'
 MENUITEMS = (
-    ('Home', 'http://www.normalesup.org/~giordano'),
+    ('About', '/'),
 )
 CC_LICENSE = 'CC-BY-NC'
 
@@ -68,15 +68,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Inria project-team Ibis', 'https://team.inria.fr/ibis'),
-         ('Dynamic Ecology', 'https://dynamicecology.wordpress.com'),
-         ('(fr) Bioinfo-fr', 'https://bioinfo-fr.net'),)
+#LINKS = (('Inria project-team Ibis', 'https://team.inria.fr/ibis'),
+#         ('Dynamic Ecology', 'https://dynamicecology.wordpress.com'),
+#         ('(fr) Bioinfo-fr', 'https://bioinfo-fr.net'),)
 
 # Social widget
 SOCIAL = (('Github', 'https://github.com/nigiord'),
           ('Google Scholar', 'https://scholar.google.fr/citations?user=r4wI-i4AAAAJ'),
           ('Orcid', 'https://orcid.org/0000-0003-2549-6631'))
 
+GITHUB_USER = 'nigiord'
 DEFAULT_PAGINATION = 10
 LOAD_CONTENT_CACHE = False
 DELETE_OUTPUT_DIRECTORY = True
